63b38255468f1d7b12157643f42591732dec7cbd
Roberto Gomes <robertinho258.rg@gmail.com>
2023-10-24 16:54:54 -0300

    add custum logger

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/robertinho258/ProjetoGo/router"
+import (
+	"github.com/robertinho258/ProjetoGo/config"
+	"github.com/robertinho258/ProjetoGo/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config inictializar error : %v", err)
+		return
+	}
 
 	//Rota Inicial
 	router.Inicio()
